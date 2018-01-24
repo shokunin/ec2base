669bea82c5fa234837857bb1dcc1fb04a0c8a377
Christian Mague <cmague@konghq.com>
2018-01-24 13:30:07 -0800

    setup bashprompt

--- manifests/bashrc.pp
+++ manifests/bashrc.pp
@@ -2,5 +2,18 @@
 # ===========================
 class ec2base::bashrc inherits ec2base {
 
+  if $::virtual == 'virtualbox' {
+    $base_user = 'vagrant'
+  } else {
+    $base_user = 'ubuntu'
+  }
+
+  file { "/home/${base_user}/.bashrc":
+    ensure => present,
+    owner  => root,
+    group  => root,
+    mode   => '0644',
+    source => 'puppet:///modules/ec2base/bash.bashrc',
+  }
 
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,6 @@
 # ===========================
 class ec2base {
 
-  notify { "class ec2base included": }
-
   include ec2base::packages
   include ec2base::bootstrap
   include ec2base::bashrc
