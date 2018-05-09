0ba4304074a76f83b6bd3bb875cdb4cb23b727e7
Chris Mague <github@mague.com>
2018-05-09 16:50:19 -0700

    remove testing

--- manifests/sysctl.pp
+++ manifests/sysctl.pp
@@ -4,8 +4,6 @@ class ec2base::sysctl inherits ec2base {
 
   $sysctl_entries = hiera_hash('sysctl', {})
 
-  notify {"DTM: $sysctl_entries": }
-
   file { '/etc/sysctl.conf':
     ensure  => present,
     owner   => root,
